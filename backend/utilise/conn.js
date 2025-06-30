b27ce5dc7c0d524dc3e3b787ba28d1b78dd861c2
Ankit Baghel <144383065+AnkitBaghel93@users.noreply.github.com>
2025-06-30 19:19:50 +0530

    Update conn.js

--- backend/utilise/conn.js
+++ backend/utilise/conn.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const conn = async () => {
   try {
-    await mongoose.connect("mongodb+srv://xtylishab000:a3pwYj66637J9wNY@cluster0.qsgtw7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully!");
   } catch (error) {
     console.error("Database connection failed:", error.message);
